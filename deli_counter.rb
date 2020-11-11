06491213458a245caaa874da9d10b0fb70c5a7f3
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-10 18:36:33 -0700

    commit tree e3ba3fc0f74a0a53c83ac6a1f1aabcea9b0cccac

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def line(deli)
+    if deli.length > 0
+        new_line = []
+        number_in_line = 1
+        deli.each do |name|
+            new_line.push("#{number_in_line}. #{name}")
+            number_in_line+=1
+        end 
+        puts "The line is currently: #{new_line.join(" ")}"
+    else 
+        puts "The line is currently empty."
+    end
+end
+
+def take_a_number(deli,name) 
+    deli << name
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
+end 
+
+def now_serving(deli)
+    if deli.length > 0 
+            puts "Currently serving #{deli[0]}."
+            deli.delete(deli[0])
+    else 
+        puts "There is nobody waiting to be served!"
+    end 
+end
+
